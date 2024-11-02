068d1ae7b5bcebc4a8f78e968c2f163181d6188d
iftakher99 <19tonmoy99@gmail.com>
2024-11-02 08:04:29 +0600

    added build

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,7 +3,11 @@ import PostPetSection from './PostPetSection';
 
 const Services = () => {
   return (
-    <div className="main-container bg-gradient-to-b from-white via-gray-50 to-blue-100 grid grid-cols-1 md:grid-cols-2">
+    <div
+      className="main-container bg-gradient-to-b
+     from-white via-gray-50 to-blue-100
+      grid grid-cols-1 md:grid-cols-2"
+    >
       <div className="adopt-pet">
         <AdoptSection />
       </div>
