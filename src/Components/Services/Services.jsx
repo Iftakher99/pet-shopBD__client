814367a316a33e00a4dd54a44827e54ab449c217
iftakher99 <19tonmoy99@gmail.com>
2024-11-03 20:43:40 +0600

    responsive

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,16 +3,15 @@ import PostPetSection from './PostPetSection';
 
 const Services = () => {
   return (
-    <div
-      className="main-container bg-gradient-to-b
-     from-white via-gray-50 to-blue-100
-      grid grid-cols-1 md:grid-cols-2"
-    >
-      <div className="adopt-pet">
-        <AdoptSection />
-      </div>
-      <div className="post-pet">
-        <PostPetSection />
+    <div className="main-container bg-gradient-to-b from-white via-gray-50 to-blue-100 p-4 md:p-8">
+      <h2 className="text-3xl font-semibold text-center mb-10">Our Services</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="adopt-pet flex flex-col w-full">
+          <AdoptSection />
+        </div>
+        <div className="post-pet flex flex-col w-full">
+          <PostPetSection />
+        </div>
       </div>
     </div>
   );
